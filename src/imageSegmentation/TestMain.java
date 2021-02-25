3271482dc7d6e827510826b8bc3fa452d1c6f71d
jf4120nf <jf4120nf@192.168.1.19>
2021-02-25 16:33:09 -0600

    added generateHistogram method

--- src/imageSegmentation/TestMain.java
+++ src/imageSegmentation/TestMain.java
@@ -1,14 +1,42 @@
 package imageSegmentation;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class TestMain {
 
 	public TestMain() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		generateHistogram();
 		
 	}
+	
+	public static void generateHistogram() throws IOException {
+		int[] histogramArray = new int[256];
+
+        File imageFile = new File("src/imageSegmentation/image1.jpg");
+        BufferedImage image = ImageIO.read(imageFile);
+
+        for(int h = 0; h < image.getHeight(); h++)
+        {
+            for(int w = 0; w < image.getWidth(); w++)
+            {
+                Color c = new Color(image.getRGB(w, h));
+                histogramArray[c.getRed()]++;
+            }
+        }
+        
+        for(int i = 0; i<histogramArray.length; i++) {
+        	System.out.println(histogramArray[i]);
+        }
+	} //end generateHistogram()
 
 }
